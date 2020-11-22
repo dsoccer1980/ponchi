b1d2d2e4fa7eea11ab087fb265ed1aa567a567c3
deniss.vorosnin <deniss.vorosnin@aktors.ee>
2020-11-22 16:01:42 +0200

    Gallery capcake

--- js/initegallery-include.js
+++ js/initegallery-include.js
@@ -70,5 +70,39 @@
 						slider_textpanel_enable_description: false,		//enable the description 
 						slider_textpanel_padding_bottom:20,				//textpanel padding bottom
 					});
+					
+					jQuery("#gallery5").unitegallery({
+						gallery_theme: "slider",
+						gallery_autoplay:false,	
+						gallery_play_interval: 30000,
+						slider_enable_text_panel: true,
+						slider_textpanel_always_on: true,
+
+						gallery_skin:"default",
+
+						gallery_width:400,							
+						gallery_height:400,						
+						gallery_min_width: 100,						
+						gallery_min_height: 100,	
+						slider_textpanel_enable_description: false,		//enable the description 
+						slider_textpanel_padding_bottom:20,				//textpanel padding bottom
+					});
+					
+					jQuery("#gallery6").unitegallery({
+						gallery_theme: "slider",
+						gallery_autoplay:false,	
+						gallery_play_interval: 30000,
+						slider_enable_text_panel: true,
+						slider_textpanel_always_on: true,
+
+						gallery_skin:"default",
+
+						gallery_width:400,							
+						gallery_height:400,						
+						gallery_min_width: 100,						
+						gallery_min_height: 100,	
+						slider_textpanel_enable_description: false,		//enable the description 
+						slider_textpanel_padding_bottom:20,				//textpanel padding bottom
+					});
 				});
 				
